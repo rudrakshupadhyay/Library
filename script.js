451993fe8952ee35756059be3dfab10a8e6817e1
rudrakshupadhyay <218912408+rudrakshupadhyay@user.noreply.github.com>
2026-01-18 22:29:03 +0530

    add class

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const myLibrary = [];
-function Book(id,title,author,pages,read){
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book{
+    constructor(id,title,author,pages,read){
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 function addBookToLibrary(title,author,pages,read){
     const id = crypto.randomUUID();
